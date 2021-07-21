f6222202e03473d6ada191d26eb0066bf2e0f1f8
txabiii <82606895+txabiii@users.noreply.github.com>
2021-07-21 14:38:22 +0800

    script for nearby hospital input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var map;
 
 var removeMode = false;
 var addMode = false;
+var hospitalMode = false;
 var addMarkerEvent;
 var currentCluster = 'none';
 
@@ -17,15 +18,24 @@ var searchInput = document.getElementById("searchInput");
 var exportBox = document.getElementById("export");
 var canvasContainer = document.getElementById("canvas-container");
 
+
 var myLatLng = JSON.parse(localStorage.getItem("myLatLng") || "[]");
 var patientLocations = JSON.parse(localStorage.getItem("patientLocations") || "[]");
+var hospitalArray = JSON.parse(localStorage.getItem("hospitalArray") || "[]");
 var polygonArray = [];
 var clusterArray = [];
+
+persondistance = JSON.parse(localStorage.getItem("persondistance") || "0.00065");;
 
 patientLocations.sort((a,b) => b.id - a.id);
 var counter;
 if(!patientLocations[0]) counter = 0; 
 else counter = patientLocations[0].id;
+
+hospitalArray.sort((a,b) => b.id - a.id);
+var hospitalCounter = 0;
+if(!hospitalArray[0]) hospitalCounter = 0; 
+else hospitalCounter = hospitalArray[0].id;
 
 function initMap() {
     geocoder = new google.maps.Geocoder();
@@ -44,7 +54,10 @@ function initMap() {
     for(i = 0; i < patientLocations.length; i++){ 
         createMarker(patientLocations[i], patientLocations[i].id);
     }
-    printAddress();
+
+    for(i = 0; i < hospitalArray.length; i++){
+        createHospital(hospitalArray[i], hospitalArray[i].id);
+    }
 
     cluster();
 
@@ -116,6 +129,7 @@ function getAddress(lat, lng, callback, i) {
         latLng: new google.maps.LatLng(lat,lng)
     }, function(responses) {
         if (responses && responses.length > 0) {
+            
             if(responses[0].address_components[1].types[0] == "route"){
                 var street = responses[0].address_components[1].long_name;
             }
@@ -146,13 +160,16 @@ function cluster(){
         var j;
         for(j = 0; j < i; j++){ // O of average of n 
             var length = Math.sqrt(Math.pow((result[j].lng - result[i].lng), 2) + Math.pow((result[j].lat - result[i].lat), 2));
-            if(length < 0.00065){
+            if(length < persondistance){
                 union(result[i], result[j]); // O of amortized n
             }
         }
     }
 
-    var clusterId = [... new Set(result.map(x => x.parent.rank))];
+    var clusterId = [... new Set(result.map(x => {
+        var root = find(x);
+        return root.rank;
+    }))];
 
     for(i = 0; i <clusterId.length; i++){ // O of no. of clusters
         var clusterPoints = result.filter(function(point){ // O of n
@@ -163,6 +180,7 @@ function cluster(){
         var newHull = convexHull(clusterPoints);  // O of subset of n that is part of the cluster x hull points .... O(nh) jarvis march
         createHullPolygon(newHull); // O of 1
     }
+    
 
     if(currentCluster != 'none') displayClusterData(currentCluster);
     drawPolygons();
@@ -207,7 +225,6 @@ function union(point1, point2){
 //function for creating the marker
 
 function createMarker(position, id){
-    //console.log(position);
     const marker = new google.maps.Marker({
         position: position,
         draggable: true,
@@ -224,7 +241,7 @@ function createMarker(position, id){
             
             marker.setMap(null);
 
-            printAddress();
+            //printAddress();
             cluster();
         } else {
             getAddress(marker.getPosition().lat(), marker.getPosition().lng(), function(address, street, i){
@@ -238,14 +255,59 @@ function createMarker(position, id){
         const position = patientLocations.findIndex(x => x.id == id);
         patientLocations[position].lat = marker.getPosition().lat();
         patientLocations[position].lng = marker.getPosition().lng(); 
+
         getAddress(marker.getPosition().lat(), marker.getPosition().lng(), function(address, street, i){
             patientLocations[position].address = address;
             patientLocations[position].street = street;
             infowindow.setContent("(" + id + ") " + address);
-            printAddress();
+            //printAddress();
         });
         cluster();
     });
+}
+
+function createHospital(latLng, id){
+    const marker = new google.maps.Marker({
+        position: latLng,
+        draggable: true,
+        icon: "images/hospital.png",
+        map,
+    });
+
+    const hospital = hospitalArray.find(hospital => hospital.id == id); 
+
+    const contentString = '<input class="hospital-name-input" id="hospital-' + id + '" placeholder="Insert hospital name. Press enter to submit" onchange="changeHospitalName(' + id + ')" value="' + hospital.name + '"/>';
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
+
+    marker.addListener("click", () => {
+        if(removeMode){
+            hospitalCounter--;
+            const locationIndex = hospitalArray.findIndex(x => x.id == id);
+            hospitalArray.splice(locationIndex, 1);
+            
+            marker.setMap(null);
+
+            if(currentCluster != 'none') displayClusterData(currentCluster);
+        } else {
+            infowindow.open(marker.get("map"), marker);
+        }  
+    });
+    google.maps.event.addListener(marker, 'dragend', function() {
+        const hospital = hospitalArray.find(hospital => hospital.id == id); 
+        hospital.lat = marker.getPosition().lat();
+        hospital.lng = marker.getPosition().lng(); 
+    });
+
+    if(currentCluster != 'none') displayClusterData(currentCluster);
+}
+
+function changeHospitalName(id){
+    const newName = document.getElementById("hospital-" + id).value;
+    const hospital = hospitalArray.find(hospital => hospital.id == id); 
+    hospital.name = newName;
+    if(currentCluster != 'none') displayClusterData(currentCluster);
 }
 
 //functions for google maps polygon
@@ -273,7 +335,7 @@ function createHullPolygon(points) {
 
     polygon.setOptions({strokeColor: color, fillColor: color});
 }
-
+//done
 function displayClusterData(i){
     if(clusterArray[i] === undefined){
         dataBox.style.display = "none";
@@ -317,37 +379,46 @@ function displayClusterData(i){
         var container = document.getElementById("nearby-hospital");
         container.innerHTML='';
 
-        for(var i=0; i<result.length; i++){
-            hospitalLatLng = {lat: result[i].geometry.location.lat(), lng: result[i].geometry.location.lng()};
-            console.log(result[i].geometry.location.lng());
+        for(var i = 0; i < hospitalArray.length; i++){
+            hospitalLatLng = {lat: hospitalArray[i].lat, lng: hospitalArray[i].lng};
 
-            hospitalName = result[i].name;
-            hospitalLocation = result[i].vicinity;
+            hospitalName = hospitalArray[i].name;
 
             var hospitalDetails = document.createElement("div");
-            hospitalDetails.setAttribute("class", "hospital-details");
+            hospitalDetails.setAttribute("class", "hospital-name");
             hospitalDetails.setAttribute("onclick", "goToHospital(" + JSON.stringify(hospitalLatLng) + ")");
-
-            var hospitalNameDiv = document.createElement("div");
-            hospitalNameDiv.setAttribute("class", "hospital-name");
-            hospitalNameDiv.innerHTML = hospitalName;     
-            
-            var hospitalLocationDiv = document.createElement("div");
-            hospitalLocationDiv.setAttribute("class", "hospital-location");
-            hospitalLocationDiv.innerHTML = hospitalLocation;
-
-            hospitalDetails.append(hospitalNameDiv);
-            hospitalDetails.append(hospitalLocationDiv);
+            hospitalDetails.innerHTML = "• " + hospitalName;     
 
             container.append(hospitalDetails);
         }
+
+        container.append(document.createElement("br"));
+
+        for(var i=0; i<result.length-1; i++){
+            hospitalLatLng = {lat: result[i].geometry.location.lat(), lng: result[i].geometry.location.lng()};
+
+            hospitalName = result[i].name;
+
+            var hospitalDetails = document.createElement("div");
+            hospitalDetails.setAttribute("class", "hospital-name");
+            hospitalDetails.setAttribute("onclick", "goToHospital(" + JSON.stringify(hospitalLatLng) + ")");
+            hospitalDetails.innerHTML = "• " + hospitalName + "*";     
+
+            container.append(hospitalDetails);
+        }
+        var note = document.createElement("small");
+        note.setAttribute("class", "note");
+        note.innerHTML = "*loaded from Google Maps Places API";
+        container.append(note);
     });
 }
 
+//done
 function goToHospital(latLng){
     map.panTo(latLng);
     map.setZoom(20);
 }
+//done
 function getNearestHospitals(points, callback){
     var hospitals = [];
 
@@ -370,13 +441,13 @@ function getNearestHospitals(points, callback){
         }
     });
 }
-
+//done
 function getCenter(points){
     var latAverage = points.reduce((a, b) => a + b.lat, 0) / points.length;
     var lngAverage = points.reduce((a, b) => a + b.lng, 0) / points.length;
     return {lat: latAverage, lng: lngAverage};
 }
-
+//done
 function closeData(){
     dataBox.style.display = "none";
     currentCluster = 'none';
@@ -395,7 +466,7 @@ function createClusterDropdown(){
         clusterDropdown.append(clusterButton);
     }
 }
-
+//done
 function viewPolygon(i){
     var bounds = polygonArray[i - 1].getBounds();
     map.fitBounds(bounds, 100);
@@ -448,7 +519,7 @@ function isLeft(a, b, c){
 }
 
 // functions for area
-
+//done
 function setArea(){
     lat = map.getCenter().lat();
     lng = map.getCenter().lng();
@@ -469,17 +540,17 @@ function setArea(){
     //add code to save  mylatlng to a storage
     //para pag niload ni user ang map ay maipapakita ang nakasave na location imbes na kung saan lang
 }
-
+//done
 function goToArea(i){
     map.panTo(myLatLng[i]);
     map.setZoom(myLatLng[i].zoom);
 }
-
+//done
 function removeArea(a){
     myLatLng.splice(a, 1);
     createAreaDropdown();
 }
-
+//done
 function createAreaDropdown() {
     dropdown.innerHTML = "";
 
@@ -516,7 +587,13 @@ function createAreaDropdown() {
 
 //functions for add and remove
 
-function addMarker(){
+function addMarker(type){
+    if(type == 'patient'){
+        hospitalMode = false;
+    } else if (type == 'hospital'){
+        hospitalMode = true;
+    }
+
     if(addMode){
         addMode = false;
         searchInput.style.display = "none";
@@ -524,28 +601,32 @@ function addMarker(){
         google.maps.event.removeListener(addMarkerEvent);
         return;
     }
+
     addMode = true;
     removeMode = false;
     
     searchInput.style.display = "flex";
     messageStyle.style.display = "block";
-    messageStyle.innerHTML = "You are in Add Mode. Click 'Add' again to exit";
+    messageStyle.innerHTML = "You are in Add " + type.charAt(0).toUpperCase() + type.slice(1) + " Mode. Click '" + type.charAt(0).toUpperCase() + type.slice(1) + "' again to exit";
     addMarkerEvent = map.addListener("click", (mapsMouseEvent) =>{
-        counter++; // index of available space in array or last index
-
-        createMarker({lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng()}, counter);
-        
-        patientLocations.push({id: counter, lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng()});
-
-        cluster();  
-
-        getAddress(mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng(), function(address, street, i){
-            //these codes are inside the callback function
-            //because we have to make sure that the geocoding is done before proceeding
-            patientLocations[i-1].address = address;
-            patientLocations[i-1].street = street;
-            printAddress(); 
-        }, counter);
+        if(type == 'patient'){
+            counter++; // index of available space in array or last index
+            createMarker({lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng()}, counter);
+            patientLocations.push({id: counter, lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng()});
+            cluster();    
+            
+            getAddress(mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng(), function(address, street, i){
+                //these codes are inside the callback function
+                //because we have to make sure that the geocoding is done before proceeding
+                patientLocations[i-1].address = address;
+                patientLocations[i-1].street = street;
+                //printAddress(); 
+            }, counter);
+        } else if (type == 'hospital'){
+            hospitalCounter++;
+            hospitalArray.push({id: hospitalCounter, lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng(), name: ''});
+            createHospital({lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng()}, hospitalCounter);
+        }
     });
 }
 
@@ -569,6 +650,8 @@ function removeMarker(){
 function saveData(){
     localStorage.setItem("patientLocations", JSON.stringify(patientLocations));
     localStorage.setItem("myLatLng", JSON.stringify(myLatLng));
+    localStorage.setItem("persondistance", JSON.stringify(persondistance));
+    localStorage.setItem("hospitalArray", JSON.stringify(hospitalArray));
 
     saveMessageBox.style.display = "flex";
     saveMessage.innerHTML = "Your progress has been saved.";
@@ -597,4 +680,25 @@ function saveImage(){
 
 function closeExportBox(){
     exportBox.style.display = "none";
+}
+
+function togglePopup(){
+    document.getElementById("distancepopup").classList.toggle("active");
+    document.getElementById("currentvalue").innerHTML = persondistance;
+}
+
+function changeDistance(){
+    persondistance = document.getElementById("currentval").value;
+    document.getElementById("currentvalue").innerHTML = persondistance;
+
+    cluster();
+
+    persondistance = document.getElementById("distance-range").value = persondistance * 100000;
+}
+
+function changeDistanceFromSlider(){
+    persondistance = document.getElementById("distance-range").value * 0.00001;
+
+    document.getElementById("currentvalue").innerHTML = persondistance;
+    cluster();
 }
